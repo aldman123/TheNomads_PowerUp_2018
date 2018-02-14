6878d18fa94749b72592a48beed80504bfc875c9
Caelan <caelan.hennig@gmail.com>
2018-02-13 20:24:30 -0700

    Started Intake Subsystem and made some changes to Lift Subsystem.

--- src/org/usfirst/frc/team5630/robot/subsystems/IntakeSubsystem.java
+++ src/org/usfirst/frc/team5630/robot/subsystems/IntakeSubsystem.java
@@ -1,11 +1,9 @@
 package org.usfirst.frc.team5630.robot.subsystems;
 
 import org.usfirst.frc.team5630.robot.RobotMap;
-import org.usfirst.frc.team5630.robot.commands.DriveRobot;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
@@ -13,7 +11,7 @@ import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 /**
  *
  */
-public class LiftSubsystem extends Subsystem {
+public class IntakeSubsystem extends Subsystem {
 
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
@@ -23,10 +21,9 @@ public class LiftSubsystem extends Subsystem {
 	DifferentialDrive robotDrive;
 
 	public void init() {
-		srx_left = new WPI_TalonSRX(RobotMap.liftMotorLeft);
-		srx_right = new WPI_TalonSRX(RobotMap.liftMotorRight);
-		
-		
+		srx_left = new WPI_TalonSRX(RobotMap.intakeMotorLeft);
+		srx_right = new WPI_TalonSRX(RobotMap.intakeMotorRight);
+				
 		m_left = new SpeedControllerGroup(srx_left);
 		m_right = new SpeedControllerGroup(srx_right);
 		
@@ -34,14 +31,9 @@ public class LiftSubsystem extends Subsystem {
 		robotDrive.setSafetyEnabled(true);
 	}
 	
-	public void teleopDrive(Joystick button) {
-		button.whileHeld(new LiftingThing());// TODO: Name command.
-		//Use a button? Two buttons for both ways?
-	}
-	
 	public void initDefaultCommand() {
-		// Set the default command for a subsystem here.
-		//setDefaultCommand(new MySpecialCommand());
-	}
+        // Set the default command for a subsystem here.
+        //setDefaultCommand(new MySpecialCommand());
+    }
 }
 
