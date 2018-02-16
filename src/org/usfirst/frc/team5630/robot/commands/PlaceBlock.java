9ab0cee7526c7c5f753abab8329205c6d1fe952e
aldman123 <alexander@aldridge.ca>
2018-02-16 16:45:55 -0700

    Added PlaceBlock command group

---
+++ src/org/usfirst/frc/team5630/robot/commands/PlaceBlock.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team5630.robot.commands;
+
+import org.usfirst.frc.team5630.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+/**
+ * @author Alexander Aldridge
+ * This command group places a cube in autonomous.
+ */
+public class PlaceBlock extends CommandGroup {
+
+    public PlaceBlock() {
+    	addSequential(new AutoLiftUp());
+    	addSequential(new AutoDrive(1, RobotMap.speedSuperSlow));
+    	//addSequential(new IntakeOut());
+    	addWequential(new AutoDrive(-1, RobotMap.speedMedium));
+    }
+}
